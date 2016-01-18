ce80da2c154e6d97eb587fcf86be30630a350075
Ian White <ian.white@t-3.com>
2016-01-18 15:19:20 -0600

    update sort function and timeout value

--- js/script.js
+++ js/script.js
@@ -1,24 +1,35 @@
-var poller = spredfast.Poller;
+(function() {
+	var poller = spredfast.Poller;
 
-(function getResults() {
-	$.when(
-			poller.poll({type: 'veggies', limit: 10}),
-			poller.poll({type: 'fruits', limit: 10})
-		)
-		.done(function(veggies, fruits) {
-			veggies
-			.concat(fruits)
-			.sort(function(prev, curr) {
-				return curr.count > prev.count;
+	function returnLargestByCount(a,b) {
+		  if (a.count < b.count) {
+		    return 1;
+		  }
+		  if (a.count > b.count) {
+		    return -1;
+		  }
+		  
+		  return 0;
+		}
+
+	(function getResults() {
+		$.when(
+				poller.poll({type: 'veggies', limit: 10}),
+				poller.poll({type: 'fruits', limit: 10})
+			)
+			.done(function(veggies, fruits) {
+				veggies
+				.concat(fruits)
+				.sort(returnLargestByCount)
+				.slice(0, 5)
+				.forEach(function(el) {
+					console.log(el.name + ' ' + el.count);
+				})
 			})
-			.slice(0, 5)
-			.forEach(function(el) {
-				console.log(el.name + ' ' + el.count);
-			})
-		})
-		.fail(function() {
-			console.warn('uh oh');
-		});
-	
-	setTimeout(getResults, 5000);
+			.fail(function() {
+				console.warn('uh oh');
+			});
+		
+		setTimeout(getResults, 15000);
+	})();
 })();
