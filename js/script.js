c665e9e1941bad77b0dc04e9dd4fd34b66d34fcd
Ian White <ian.white@t-3.com>
2016-01-19 11:56:14 -0600

    include eventemitter and document ready

--- js/script.js
+++ js/script.js
@@ -1,35 +1,38 @@
-(function() {
-	var poller = new spredfast.Poller();
+$(document).ready(function() {
+	(function() {
+		var poller = new spredfast.Poller(),
+				EVT = new EventEmitter2();
 
-	function returnLargestByCount(a,b) {
-	  if (a.count < b.count) {
-	    return 1;
-	  }
-	  if (a.count > b.count) {
-	    return -1;
-	  }
-	  
-	  return 0;
-	}
+		function returnLargestByCount(a,b) {
+		  if (a.count < b.count) {
+		    return 1;
+		  }
+		  if (a.count > b.count) {
+		    return -1;
+		  }
+		  
+		  return 0;
+		}
 
-	(function getResults() {
-		$.when(
-				poller.poll({type: 'veggies', limit: 10}),
-				poller.poll({type: 'fruits', limit: 10})
-			)
-			.done(function(veggies, fruits) {
-				veggies
-				.concat(fruits)
-				.sort(returnLargestByCount)
-				.slice(0, 5)
-				.forEach(function(el) {
-					console.log(el.name + ' ' + el.count);
+		(function getResults() {
+			$.when(
+					poller.poll({type: 'veggies', limit: 10}),
+					poller.poll({type: 'fruits', limit: 10})
+				)
+				.done(function(veggies, fruits) {
+					veggies
+					.concat(fruits)
+					.sort(returnLargestByCount)
+					.slice(0, 5)
+					.forEach(function(el) {
+						console.log(el.name + ' ' + el.count);
+					})
 				})
-			})
-			.fail(function() {
-				console.warn('uh oh');
-			});
-		
-		setTimeout(getResults, 15000);
+				.fail(function() {
+					console.warn('uh oh');
+				});
+			
+			// setTimeout(getResults, 15000);
+		})();
 	})();
-})();
+});
