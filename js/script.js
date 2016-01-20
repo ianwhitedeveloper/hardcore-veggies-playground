e9644e40d079696378a0cdee68588d9e835fffe8
Ian White <ian.white@t-3.com>
2016-01-19 22:33:31 -0600

    update observable solution

--- js/script.js
+++ js/script.js
@@ -1,101 +1,129 @@
-(function() {
-	var Observable = Rx.Observable;
-	var poller = new spredfast.Poller();
+$(document).ready(function() {
+	(function() {
+		var Observable = Rx.Observable,
+				poller = new spredfast.Poller(),
+				EVT = new EventEmitter2(),
+				template = document.getElementById('template').innerHTML,
+				leaderboardListElement = $('[rel="js_produce_leaderboard_list"]');
 
-	function returnLargestByCount(a,b) {
-	  if (a.count < b.count) {
-	    return 1;
-	  }
-	  if (a.count > b.count) {
-	    return -1;
-	  }
-	  
-	  return 0;
-	}
+		Mustache.parse(template);
 
-	function getCategory(category, limit) {
-	    return Observable.create(function forEach(observer) {
-	        poller.poll({type: category, limit:limit})
-		      .then(function(data) {
-            observer.onNext(data);
-            observer.onCompleted();
-	        })
-	        .fail(function (err) {
-            observer.onError(err);
-	        })
-	    });
-	}
+		EVT.on('renderItemsToLeaderboard', renderItemsToLeaderboard);
+		EVT.on('retrieveResultsFailure', retrieveResultsFailure);
 
-	function onNext(data) {
-		console.log(data);
-	}
+		function returnLargestByCount(a,b) {
+		  if (a.count < b.count) {
+		    return 1;
+		  }
+		  if (a.count > b.count) {
+		    return -1;
+		  }
+		  
+		  return 0;
+		}
 
-	function onError(err) {
-		console.error(err);
-	}
+		function formatNumber(num) {
+			return num.toString().replace(/(\d)(?=(\d{3})+(?!\d))/g, "$1,");
+		}
 
-	function onCompleted() {
-		console.log('completed');
-	}
+		function renderItemsToLeaderboard(items) {
+			var data = { 'items' : items};
+			leaderboardListElement.html(Mustache.render(template, data));
+		}
 
-	////////////////
-	// Solution 1 //
-	////////////////
-	
-	var source1 = Observable.fromPromise(poller.poll({type: 'fruits', limit:10}));
-	var source2 = Observable.fromPromise(poller.poll({type: 'veggies', limit:10}));
+		function retrieveResultsFailure(err) {
+			console.error(err);
+		}
 
-	(function getResults() {
-		Observable.
-			zip(
-				source1, 
-				source2, 
-				(fruits,veggies) => 
-					fruits.concat(veggies)
-					.sort(returnLargestByCount)
-					.slice(0,5)
-			)
-			.subscribe(onNext, onError, onCompleted);
+		function getCategory(category, limit) {
+		    return Observable.create(function forEach(observer) {
+		        poller.poll({type: category, limit:limit})
+			      .then(function(data) {
+	            observer.onNext(data);
+	            observer.onCompleted();
+		        })
+		        .fail(function (err) {
+	            observer.onError(err);
+		        })
+		    });
+		}
+
+		function onNext(items) {
+			console.log(items);
+			EVT.emit('renderItemsToLeaderboard', items);
+		}
+
+		function onError(err) {
+			console.error(err);
+			EVT.emit('retrieveResultsFailure', err);
+		}
+
+		function onCompleted() {
+			console.log('completed');
+		}
+
+		////////////////
+		// Solution 1 //
+		////////////////
+		
+		var source1 = Observable.fromPromise(poller.poll({type: 'fruits', limit:10}));
+		var source2 = Observable.fromPromise(poller.poll({type: 'veggies', limit:10}));
+
+		(function getResults() {
+			Observable.
+				zip(
+					source1, 
+					source2, 
+					(fruits,veggies) => 
+						fruits.concat(veggies)
+						.sort(returnLargestByCount)
+						.slice(0,5)
+						.map(function(el) {
+							return {"name": el.name, "count": formatNumber(el.count)};
+						})
+				)
+				.subscribe(onNext, onError, onCompleted);
 
 
-		// setTimeout(getResults, 15000);
+			// setTimeout(getResults, 15000);
+		})();
+		
+		////////////////
+		// Solution 2 //
+		////////////////
+		/*
+		(function getResults() {
+			Observable.
+				zip(
+					getCategory('fruits' ,10), 
+					getCategory('veggies' ,10), 
+					(fruits,veggies) => 
+						fruits.concat(veggies)
+						.sort(returnLargestByCount)
+						.slice(0,5)
+				)
+				.subscribe(onNext, onError, onCompleted);
+
+			// setTimeout(getResults, 15000);
+		})();
+		*/
+
+		////////////////
+		// Solution 3 //
+		////////////////
+		/*
+		(function getResults() {
+			getCategory('fruits', 10)
+		    .concatMap(function(x) {
+		        return getCategory('veggies', 10)
+		        .map(function(y) {
+		            return x.concat(y).sort(returnLargestByCount).slice(0,5);
+		        })
+		    })
+				.subscribe(onNext, onError, onCompleted);
+
+
+			// setTimeout(getResults, 15000);
+		})();*/
 	})();
-	
-	////////////////
-	// Solution 2 //
-	////////////////
-	/*
-	(function getResults() {
-		Observable.
-			zip(
-				getCategory('fruits' ,10), 
-				getCategory('veggies' ,10), 
-				(fruits,veggies) => 
-					fruits.concat(veggies)
-					.sort(returnLargestByCount)
-					.slice(0,5)
-			)
-			.subscribe(onNext, onError, onCompleted);
-
-		// setTimeout(getResults, 15000);
-	})();
-	*/
-
-	////////////////
-	// Solution 3 //
-	////////////////
-	/*
-	(function getResults() {
-		getCategory('fruits', 10)
-	    .concatMap(function(x) {
-	        return getCategory('veggies', 10)
-	        .map(function(y) {
-	            return x.concat(y).sort(returnLargestByCount).slice(0,5);
-	        })
-	    })
-			.subscribe(onNext, onError, onCompleted);
-
-
-		// setTimeout(getResults, 15000);
-	})();*/
-})();
+});
