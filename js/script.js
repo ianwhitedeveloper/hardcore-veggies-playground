b83028ab1b17e1ab09cdce55059cd762fbb218b8
Ian White <ian.white@t-3.com>
2016-01-19 22:21:26 -0600

    update module solution

--- js/script.js
+++ js/script.js
@@ -1,7 +1,16 @@
-(function() {
-	var poller = spredfast.Poller;
+$(document).ready(function() {
+	(function() {
+		var poller = spredfast.Poller,
+				EVT = new EventEmitter2(),
+				template = document.getElementById('template').innerHTML,
+				leaderboardListElement = $('[rel="js_produce_leaderboard_list"]');
 
-	function returnLargestByCount(a,b) {
+		Mustache.parse(template);
+
+		EVT.on('renderItemsToLeaderboard', renderItemsToLeaderboard);
+		EVT.on('retrieveResultsFailure', retrieveResultsFailure);
+
+		function returnLargestByCount(a,b) {
 		  if (a.count < b.count) {
 		    return 1;
 		  }
@@ -12,24 +21,41 @@
 		  return 0;
 		}
 
-	(function getResults() {
-		$.when(
-				poller.poll({type: 'veggies', limit: 10}),
-				poller.poll({type: 'fruits', limit: 10})
-			)
-			.done(function(veggies, fruits) {
-				veggies
-				.concat(fruits)
-				.sort(returnLargestByCount)
-				.slice(0, 5)
-				.forEach(function(el) {
-					console.log(el.name + ' ' + el.count);
+		function formatNumber(num) {
+			return num.toString().replace(/(\d)(?=(\d{3})+(?!\d))/g, "$1,");
+		}
+
+		function renderItemsToLeaderboard(items) {
+			var data = { 'items' : items};
+			leaderboardListElement.html(Mustache.render(template, data));
+		}
+
+		function retrieveResultsFailure(err) {
+			console.error(err);
+		}
+
+		(function getResults() {
+			$.when(
+					poller.poll({type: 'veggies', limit: 10}),
+					poller.poll({type: 'fruits', limit: 10})
+				)
+				.done(function(veggies, fruits) {
+					var items = 
+						veggies
+						.concat(fruits)
+						.sort(returnLargestByCount)
+						.slice(0, 5)
+						.map(function(el) {
+							return {"name": el.name, "count": formatNumber(el.count)};
+						});
+
+					EVT.emit('renderItemsToLeaderboard', items);
 				})
-			})
-			.fail(function() {
-				console.warn('uh oh');
-			});
-		
-		setTimeout(getResults, 15000);
+				.fail(function(err) {
+					EVT.emit('retrieveResultsFailure', err);
+				});
+			
+			setTimeout(getResults, 15000);
+		})();
 	})();
-})();
+});
